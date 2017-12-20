4ee01a15acdde0a40a745713e4bc182efd4923de
csilbergleith <git@10ringaz.com>
2017-12-20 16:53:16 -0700

    CSV Reader tweaks

--- src/csvReader.java
+++ src/csvReader.java
@@ -1,4 +1,4 @@
-//package com.csilberg.csvReader;
+// package csvReader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,7 +8,6 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
-import com.opencsv.CSVReader;
 
 public class csvReader {
     public static void main(String[] args) {
@@ -34,7 +33,7 @@ public class csvReader {
                 inputStream = new Scanner(file);
 
                 while (inputStream.hasNext()) {
-                    String line = inputStream.next();
+                    String line = inputStream.nextLine();
                     String[] values = line.split(",");
                     // this adds the currently parsed line to the 2-dimensional string array
                     lines.add(Arrays.asList(values));
@@ -47,19 +46,21 @@ public class csvReader {
 
             // the following code lets you iterate through the 2-dimensional array
             int lineNo = 1;
+            String[] colNames = {"customerId", "First Name", "Last Name", "Street1", "Street2","City", "State",
+                    "Zip","Country", "Phone1", "Phone2", "Phone3","CreateDate"};
+
+            System.out.println(colNames[0]);
             for (List<String> line : lines) {
+//                System.out.println("phone # " + line.get(9));
                 int columnNo = 1;
                 for (String value : line) {
-                    System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
+                    System.out.println("Line " + lineNo + " " + colNames[columnNo-1] + ": " + value);
+                    if (columnNo == 10) {
+                    }
                     columnNo++;
                 }
                 lineNo++;
             }
-        }
-
-        if (version == 3){
-            CSVReader reader = null;
-
         }
 
     }
