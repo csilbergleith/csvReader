697aca6744a01e9e6e8f7be21ebc09aba5394d89
csilbergleith <git@10ringaz.com>
2017-12-20 14:31:10 -0700

    updates

--- src/csvReader.java
+++ src/csvReader.java
@@ -1,4 +1,4 @@
-//package com.csilberg.csvreader;
+//package com.csilberg.csvReader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,50 +8,58 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
+import com.opencsv.CSVReader;
 
 public class csvReader {
     public static void main(String[] args) {
-        String filename = "\\Users\\csilb\\Documents\\java\\csvReader\\src\\properties.csv";
+        String filename = "\\Users\\csilb\\Documents\\java\\csvReader\\src\\sample.csv";
         File file = new File(filename);
+        int version = 2;
 
-        Properties p = System.getProperties();
-        Enumeration keys = p.keys();
-        while (keys.hasMoreElements()) {
-            String key = (String)keys.nextElement();
-            String value = (String)p.get(key);
+        if (version == 1) {
+            Properties p = System.getProperties();
+            Enumeration keys = p.keys();
+            while (keys.hasMoreElements()) {
+                String key = (String) keys.nextElement();
+                String value = (String) p.get(key);
             System.out.println(key + ": " + value);
+            }
         }
 
-//        System.out.println("Working Dir" +  System.getProperty("user.dir"));
-//        System.out.println(System.getProperties());
-        // this gives you a 2-dimensional array of strings
-        List<List<String>> lines = new ArrayList<>();
-        Scanner inputStream;
+        if (version == 2) {
+            List<List<String>> lines = new ArrayList<>();
+            Scanner inputStream;
 
-        try{
-            inputStream = new Scanner(file);
+            try {
+                inputStream = new Scanner(file);
 
-            while(inputStream.hasNext()){
-                String line= inputStream.next();
-                String[] values = line.split(",");
-                // this adds the currently parsed line to the 2-dimensional string array
-                lines.add(Arrays.asList(values));
+                while (inputStream.hasNext()) {
+                    String line = inputStream.next();
+                    String[] values = line.split(",");
+                    // this adds the currently parsed line to the 2-dimensional string array
+                    lines.add(Arrays.asList(values));
+                }
+
+                inputStream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
 
-            inputStream.close();
-        }catch (FileNotFoundException e) {
-            e.printStackTrace();
+            // the following code lets you iterate through the 2-dimensional array
+            int lineNo = 1;
+            for (List<String> line : lines) {
+                int columnNo = 1;
+                for (String value : line) {
+                    System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
+                    columnNo++;
+                }
+                lineNo++;
+            }
         }
 
-        // the following code lets you iterate through the 2-dimensional array
-        int lineNo = 1;
-        for(List<String> line: lines) {
-            int columnNo = 1;
-            for (String value: line) {
-//                System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
-                columnNo++;
-            }
-            lineNo++;
+        if (version == 3){
+            CSVReader reader = null;
+
         }
 
     }
